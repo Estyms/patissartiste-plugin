be143fdfa02b102b1a629e0a55d1652b50f786c2
evann <evann.regnault@pm.me>
2023-03-19 19:22:57 +0100

    Version 1.2.1 - Falling tree is now a module - Bow, Crosswbow and Trident can now be fireproof - Better lang management

--- src/main/java/dev/evannregnault/ida/enchantments/Fireproof.java
+++ src/main/java/dev/evannregnault/ida/enchantments/Fireproof.java
@@ -21,7 +21,7 @@ public class Fireproof extends Enchantment implements PolymerEnchantment {
                 Items.SHULKER_BOX, Items.WHITE_SHULKER_BOX, Items.LIGHT_GRAY_SHULKER_BOX, Items.GRAY_SHULKER_BOX, Items.BLACK_SHULKER_BOX, Items.BROWN_SHULKER_BOX,
                 Items.RED_SHULKER_BOX, Items.ORANGE_SHULKER_BOX, Items.YELLOW_SHULKER_BOX, Items.LIME_SHULKER_BOX, Items.GREEN_SHULKER_BOX,
                 Items.CYAN_SHULKER_BOX, Items.LIGHT_BLUE_SHULKER_BOX, Items.BLUE_SHULKER_BOX, Items.PURPLE_SHULKER_BOX, Items.MAGENTA_SHULKER_BOX, Items.PINK_SHULKER_BOX,
-                Items.ELYTRA};
+                Items.ELYTRA,  Items.BOW, Items.CROSSBOW, Items.TRIDENT};
         return Arrays.stream(validItems).anyMatch(x -> x == stack.getItem());
     }
 
